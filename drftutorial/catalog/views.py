b20cd4157d12f43b5a27187f74748961343ae3af
goldenglorys <olusolagloryolamide@gmail.com>
2021-04-22 11:27:34 +0100

    Create initail migration data and getting them through api

--- drftutorial/catalog/views.py
+++ drftutorial/catalog/views.py
@@ -1,22 +1,33 @@
+# from django.http import HttpResponse
+# from rest_framework.renderers import JSONRenderer
+# from rest_framework.parsers import JSONParser
+# from .models import Product
+# from .serializers import ProductSerializer
+
+
+# class JSONResponse(HttpResponse):
+#     """
+#     An HttpResponse that renders its content into JSON.
+#     """
+#     def __init__(self, data, **kwargs):
+#         content = JSONRenderer().render(data)
+#         kwargs['content_type'] = 'application/json'
+#         super(JSONResponse, self).__init__(content, **kwargs)
+
+
+# def product_list(request):
+#     if request.method == 'GET':
+#         products = Product.objects.all()
+#         serializer = ProductSerializer(products, many=True)
+#         return JSONResponse(serializer.data)
+
 from django.http import HttpResponse
-from rest_framework.renderers import JSONRenderer
-from rest_framework.parsers import JSONParser
+from rest_framework import generics
+from rest_framework.response import Response
 from .models import Product
 from .serializers import ProductSerializer
 
 
-class JSONResponse(HttpResponse):
-    """
-    An HttpResponse that renders its content into JSON.
-    """
-    def __init__(self, data, **kwargs):
-        content = JSONRenderer().render(data)
-        kwargs['content_type'] = 'application/json'
-        super(JSONResponse, self).__init__(content, **kwargs)
-
-
-def product_list(request):
-    if request.method == 'GET':
-        products = Product.objects.all()
-        serializer = ProductSerializer(products, many=True)
-        return JSONResponse(serializer.data)
+class ProductList(generics.ListAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
