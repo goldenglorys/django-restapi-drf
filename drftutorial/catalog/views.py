897bd15c84bbf728981a323d0642093b9cbe370b
goldenglorys <olusolagloryolamide@gmail.com>
2021-04-22 12:35:40 +0100

    Add details management, relations between models product and reviews, nested APIs and a different level of permissions

--- drftutorial/catalog/views.py
+++ drftutorial/catalog/views.py
@@ -1,12 +1,43 @@
-from .permissions import IsAdminOrReadOnly
+from .permissions import IsAdminOrReadOnly, IsOwnerOrReadOnly
 from django.http import HttpResponse
 from rest_framework import generics
 from rest_framework.response import Response
 from .models import Product
 from .serializers import ProductSerializer
+from django.http import Http404
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework import status
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from .models import Product, Review
+from .serializers import ProductSerializer, ReviewSerializer
 
 
 class ProductList(generics.ListCreateAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
-    permission_classes = (IsAdminOrReadOnly, )
+    permission_classes = (IsAdminOrReadOnly,)
+    
+class ProductDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+    permission_classes = (IsAdminOrReadOnly,)
+    
+class ReviewList(generics.ListCreateAPIView):
+    queryset = Review.objects.all()
+    serializer_class = ReviewSerializer
+    permission_classes = (IsAuthenticatedOrReadOnly, )
+
+    def perform_create(self, serializer):
+        serializer.save(
+            created_by=self.request.user,
+            product_id=self.kwargs['product_id'])
+
+class ReviewDetail(generics.RetrieveUpdateDestroyAPIView):
+    serializer_class = ReviewSerializer
+    permission_classes = (IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly)
+    lookup_url_kwarg = 'review_id'
+
+    def get_queryset(self):
+        review = self.kwargs['review_id']
+        return Review.objects.filter(id=review)
