075b5f284e26103e686b7decbb32cf5710ed31c8
goldenglorys <olusolagloryolamide@gmail.com>
2021-04-22 11:44:55 +0100

    Implement a post method using APIView class

--- drftutorial/catalog/views.py
+++ drftutorial/catalog/views.py
@@ -3,6 +3,7 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 from .models import Product
 from .serializers import ProductSerializer
+from rest_framework import status
 
 
 class ProductList(APIView):
@@ -10,3 +11,10 @@ class ProductList(APIView):
         products = Product.objects.all()
         serializer = ProductSerializer(products, many=True)
         return Response(serializer.data)
+
+    def post(self, request, format=None):
+        serializer = ProductSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUST)
