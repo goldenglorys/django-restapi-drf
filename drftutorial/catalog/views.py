16c306efc6b4796050ac6b1b6c8c4f1e8c31a586
goldenglorys <olusolagloryolamide@gmail.com>
2021-04-22 11:36:07 +0100

    using the very basic APIView class and we have explicitly defined the GET method for it

--- drftutorial/catalog/views.py
+++ drftutorial/catalog/views.py
@@ -1,33 +1,12 @@
-# from django.http import HttpResponse
-# from rest_framework.renderers import JSONRenderer
-# from rest_framework.parsers import JSONParser
-# from .models import Product
-# from .serializers import ProductSerializer
-
-
-# class JSONResponse(HttpResponse):
-#     """
-#     An HttpResponse that renders its content into JSON.
-#     """
-#     def __init__(self, data, **kwargs):
-#         content = JSONRenderer().render(data)
-#         kwargs['content_type'] = 'application/json'
-#         super(JSONResponse, self).__init__(content, **kwargs)
-
-
-# def product_list(request):
-#     if request.method == 'GET':
-#         products = Product.objects.all()
-#         serializer = ProductSerializer(products, many=True)
-#         return JSONResponse(serializer.data)
-
 from django.http import HttpResponse
-from rest_framework import generics
 from rest_framework.response import Response
+from rest_framework.views import APIView
 from .models import Product
 from .serializers import ProductSerializer
 
 
-class ProductList(generics.ListAPIView):
-    queryset = Product.objects.all()
-    serializer_class = ProductSerializer
+class ProductList(APIView):
+    def get(self, request, format=None):
+        products = Product.objects.all()
+        serializer = ProductSerializer(products, many=True)
+        return Response(serializer.data)
